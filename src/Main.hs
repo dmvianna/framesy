b91e9a78d605d0487f4401780d8f2ad2f9b2e64f
Daniel Vianna <dmlvianna@gmail.com>
2017-11-12 18:53:59 +1100

    doing simple data analysis\!

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,42 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
-
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Frames.CSV (tableTypes)
+import Control.Applicative
+import qualified Control.Foldl as L
+import qualified Data.Foldable as F
+import Data.Proxy (Proxy(..))
+
+import Pipes (Producer)
+import Lens.Micro.Extras (view)
+
+import Frames.Frame
+import Frames.InCore (inCoreAoS)
+import Frames.CSV ( tableTypes'
+                  , separator
+                  , rowGen
+                  , rowTypeName
+                  , readTableOpt
+                  , columnNames
+                  )
 import Data.Text
 
-tableTypes "Movies" "../data/ml-100k/u.user"
+-- tableTypes "Movies" "../data/ml-100k/u.user"
+
+tableTypes' rowGen { rowTypeName = "User"
+                   , columnNames = [ "user id", "age", "gender"
+                                   , "occupation", "zip code" ]
+                   , separator = "|" }
+  "../data/ml-100k/u.user"
+
+movieStream :: Producer User IO ()
+movieStream = readTableOpt userParser "../data/ml-100k/u.user"
+
+loadMovies :: IO (Frame User)
+loadMovies = inCoreAoS movieStream
 
 main :: IO ()
 main = do
